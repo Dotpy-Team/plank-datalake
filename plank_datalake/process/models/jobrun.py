12d2dce34f97c0d1f9554f6df08a923ea320f419
Luiz Cairo <57645035+luiz-g1@users.noreply.github.com>
2024-01-16 18:27:06 -0300

    ajustes PK Jobrun

--- plank_datalake/process/models/jobrun.py
+++ plank_datalake/process/models/jobrun.py
@@ -3,10 +3,10 @@ from business.models import DataSet, customer
 from process.models import Tables
 
 class JobRun(models.Model):
-    job_id = models.CharField(max_length=200)
+    job_id = models.AutoField(primary_key=True)
     table = models.ForeignKey(Tables, on_delete=models.CASCADE)
     dth_last_updated = models.DateTimeField()
     dth_start_at = models.DateTimeField()
     str_status = models.CharField(max_length=200)
     int_total_size = models.IntegerField()
-    int_total_rows = models.IntegerField()
+    int_total_rows = models.IntegerField() 
