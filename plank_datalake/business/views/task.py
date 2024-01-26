4bf365d2a57c63423470250496e74ef2f13079bd
Luiz Cairo <57645035+luiz-g1@users.noreply.github.com>
2024-01-26 02:21:05 -0300

    Task View Update

--- plank_datalake/business/views/task.py
+++ plank_datalake/business/views/task.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect, get_object_or_404
 from django.urls import reverse
 from django.http import Http404
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib import messages
 from business.models import Task
@@ -31,6 +32,7 @@ TASK PROFILE_TASK
 
 TASK_PATH = 'business/Task/'
 
+@login_required
 def new_task_by_id_customer(request,customer_id):
     customer_id = uncrip(customer_id)
     if request.method == 'POST':
@@ -55,6 +57,7 @@ def new_task_by_id_customer(request,customer_id):
         }
     return render(request, html_location, dict_form)
 
+@login_required
 def profile_task(request,task_id):
     task_id = uncrip(task_id)
     # try:
