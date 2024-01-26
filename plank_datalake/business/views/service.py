873487e412f1ec1ac782d841f0ae213d917f4d40
Luiz Cairo <57645035+luiz-g1@users.noreply.github.com>
2024-01-26 02:19:06 -0300

    Service View update

--- plank_datalake/business/views/service.py
+++ plank_datalake/business/views/service.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect, get_object_or_404
 from django.urls import reverse
 from django.http import Http404
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib import messages
 from business.models import Service
@@ -30,6 +31,7 @@ SERVICE PROFILE
 
 SERVICE_PATH = 'business/SERVICE/'
 
+@login_required
 def new_service(request):
     service_all = Service.objects.all()
     
