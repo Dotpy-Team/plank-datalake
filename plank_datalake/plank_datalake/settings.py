9df847051b3102b439db0bd6cecb7fda3ced27da
Luiz Cairo <57645035+luiz-g1@users.noreply.github.com>
2024-01-26 01:45:17 -0300

    Settings update

--- plank_datalake/plank_datalake/settings.py
+++ plank_datalake/plank_datalake/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
     }
 }
 
+LOGIN_URL = 'user_login'
+LOGIN_REDIRECT_URL = 'home_page'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
