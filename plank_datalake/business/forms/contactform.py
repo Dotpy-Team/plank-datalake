1c41879275c4011fd1c3973370f344834d49f04b
luiz-g1 <92112179+Mike301014@users.noreply.github.com>
2024-05-16 16:44:09 -0300

    Forms: Adicionando campos no forms de Contact

--- plank_datalake/business/forms/contactform.py
+++ plank_datalake/business/forms/contactform.py
@@ -3,22 +3,46 @@ from business.models import Contacts
 
 
 class ContactsForm(forms.ModelForm):
-    str_name = forms.CharField(
+    str_first_name = forms.CharField(
         label='Nome', 
         widget= forms.TextInput(attrs={'class': 'form-control'}),
         required=True
     )
 
-    email = forms.CharField(
-        label='Email',
+    str_last_name = forms.CharField(
+        label='Sobrenome',
+        widget= forms.TextInput(attrs={'class': 'form-control'}),
+        required= True
+    )
+
+    str_email = forms.CharField(
+        label='E-mail',
         widget= forms.TextInput(attrs={'class': 'form-control'}),
         required=True
+    )
+    
+    str_comercial_email = forms.CharField(
+        label='E-mail Comercial',
+        widget= forms.TextInput(attrs={'class': 'form-control'}),
+        required= False
     )
 
     str_phone_number = forms.CharField(
         label = 'Telefone',
         widget= forms.TextInput(attrs={'class': 'form-control'}),
         required=True
+    )
+
+    str_comercial_phone_number = forms.CharField(
+        label='Telefone Comercial',
+        widget= forms.TextInput(attrs={'class': 'form-control'}),
+        required=False
+    )
+
+    str_whatsapp_number = forms.CharField(
+        label='WhatsApp',
+        widget= forms.TextInput(attrs={'class': 'form-control'}),
+        required= True
     )
 
     CONTACT_CHOICES = (
@@ -36,8 +60,12 @@ class ContactsForm(forms.ModelForm):
     class Meta:
         model = Contacts
         fields = {
-            'str_name',
-            'email',
+            'str_first_name',
+            'str_last_name', 
+            'str_email',
+            'str_comercial_email',
             'str_phone_number',
+            'str_comercial_phone_number',
+            'str_whatsapp_number',
             'str_type_contact'
         }
