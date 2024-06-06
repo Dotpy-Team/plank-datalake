15975f61b96f5b4a15356116e0174a5ebf284de1
luiz-g1 <92112179+Mike301014@users.noreply.github.com>
2024-06-06 16:47:46 -0300

    Adição do separador  em columns_api

--- plank_datalake/process/views/columns_api.py
+++ plank_datalake/process/views/columns_api.py
@@ -14,18 +14,20 @@ def get_table_columns(request, table_id):
     except Tables.DoesNotExist:
         return Response('Tables not found')
     
-    tables = Tables.objects.filter(table_id=table_id).values('table_id', 'str_name')
-    
+    tables = Tables.objects.filter(table_id=table_id).values('table_id', 'str_name', 'str_separador')
+
     for row in tables:
         id = row['table_id']
         name = row['str_name']
+        sep = row['str_separador']
 
     columns = Columns.objects.filter(table_id=tables_id).values('id_column', 'str_source_name', 'str_datatype')
 
     response_dict = {
         "id": id,
         "nome": name,
-        "columns":columns
+        "sep": sep,
+        "columns": columns
     }
 
     return Response(response_dict)
