459da4752d3e1174900b6ce59a6a628348881602
luiz-g1 <57645035+luiz-g1@users.noreply.github.com>
2024-09-24 15:10:18 -0300

    alterações dos links da sidebar e adição do botão de logout (thiego ajustar)

--- plank_datalake/plank_datalake/urls.py
+++ plank_datalake/plank_datalake/urls.py
@@ -131,7 +131,7 @@ urlpatterns = [
     path('list-trigger/', p.list_trigger, name='list_trigger'),
 
     #mock dashboard
-    path('dashboard-teste/', p.dashboard, name='dashboard'),
+    path('dashboard-teste/', p.dashboard, name='dashboard_teste'),
 
     path('dashboard/', p.list_dashboard, name='dashboard'),
     path('add-dashboard/', p.add_dashboard, name='dashboard'),
