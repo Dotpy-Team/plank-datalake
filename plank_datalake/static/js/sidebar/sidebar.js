6d4be13bcd458035498ce974b2457c8174dbfc9c
Thiego Ribeiro <thiego.hl@gmail.com>
2024-09-27 20:51:30 -0300

    fonte alterada, add icones brancos, js e css para hover dos itens sidebar

--- plank_datalake/static/js/sidebar/sidebar.js
+++ plank_datalake/static/js/sidebar/sidebar.js
@@ -6,3 +6,21 @@ document.getElementById('open_btn').addEventListener('click', function () {
 
   searchButton.classList.toggle('expanded');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const items = document.querySelectorAll('.side-item');
+
+  items.forEach(item => {
+      const icon = item.querySelector('.icon');
+      const originalSrc = icon.src; // Armazena o src original
+      const hoverSrc = icon.getAttribute('data-hover-src'); // Obtém o src de hover do atributo data
+
+      item.addEventListener('mouseover', function() {
+          icon.src = hoverSrc; // Troca para a imagem de hover
+      });
+
+      item.addEventListener('mouseout', function() {
+          icon.src = originalSrc; // Volta para a imagem original
+      });
+  });
+});
